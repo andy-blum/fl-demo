a36e97ffb7a77fe9d1191dc5888d5145145b3ff2
Andy Blum <andy.blum.01@gmail.com>
2022-02-13 15:11:20 -0500

    add primary color field to theme settings

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -13,4 +13,21 @@ function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $fo
     '#title' => t('Foomami Specific Settings'),
     '#open' => true,
   ];
+
+  // Create a text input field
+  $form['foomami_settings']['primary_color'] = [
+    '#type' => 'textfield',
+    '#maxlength' => 7,
+    '#size' => 10,
+    '#title' => t('Primary Color'),
+    '#description' => t("Enter color in full hexadecimal format (#abc123).<br/>
+      Derivatives will be formed from this color."),
+    '#default_value' => theme_get_setting('primary_color'),
+    '#attributes' => [
+      'pattern' => '^#[a-fA-F0-9]{6}',
+    ],
+    '#wrapper_attributes' => [
+      'data-drupal-selector' => 'foomami-color-picker',
+    ],
+  ];
 }

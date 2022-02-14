38b28b4f86adeee963bf112e1e450376bed764d7
Andy Blum <andy.blum.01@gmail.com>
2022-02-14 11:03:58 -0500

    wire up text/color input pairs

--- web/themes/custom/foomami/js/components/fields/color.js
+++ web/themes/custom/foomami/js/components/fields/color.js
@@ -7,9 +7,29 @@
       );
 
       textInputs.forEach(textInput => {
+        // Create color input element
         const colorInput = document.createElement('input');
         colorInput.type = 'color';
 
+        // Add form-element classes to get admin theme styles
+        colorInput.classList.add(
+          'form-element',
+          'form-element--type-color',
+        );
+
+        // Set initial value from text element
+        colorInput.value = textInput.value;
+
+        // Watch for changes and keep inputs in sync
+        textInput.addEventListener('input', () => {
+          colorInput.value = textInput.value;
+        });
+
+        colorInput.addEventListener('input', () => {
+          textInput.value = colorInput.value;
+        });
+
+        // Inject color input into DOM
         textInput.after(colorInput);
       })
     },

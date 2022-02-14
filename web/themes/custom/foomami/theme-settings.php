a125b51f70ccafd4099c0d9ec5978fd2b61ef279
Andy Blum <andy.blum.01@gmail.com>
2022-02-14 09:28:39 -0500

    add library, CSS, JS for color input fields

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -7,6 +7,9 @@ use \Drupal\Core\Form\FormStateInterface;
  */
 function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
 
+  // Attach our the foomami theme-settings library
+  $form['#attached']['library'][] = 'foomami/theme_settings';
+
   // Create a collapsible details element that wraps our settings.
   $form['foomami_settings'] = [
     '#type' => 'details',

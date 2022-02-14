95282e5e2718ab421d946fe50a57afbd258c89f5
Andy Blum <andy.blum.01@gmail.com>
2022-02-14 09:36:01 -0500

    Add color input with JS

--- web/themes/custom/foomami/js/components/fields/color.js
+++ web/themes/custom/foomami/js/components/fields/color.js
@@ -1,1 +1,16 @@
-console.log('color.js')
+((Drupal) => {
+  Drupal.behaviors.foomami_color = {
+    attach: () => {
+      const textInputs = document.querySelectorAll(
+        '[data-drupal-selector="edit-primary-color"]',
+      );
+
+      textInputs.forEach(textInput => {
+        const colorInput = document.createElement('input');
+        colorInput.type = 'color';
+
+        textInput.after(colorInput);
+      })
+    },
+  };
+})(Drupal);

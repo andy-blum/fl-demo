6e9e9e31929c9ea470eb624aa8deaadbff4238d9
Andy Blum <andy.blum.01@gmail.com>
2022-02-17 16:58:51 -0500

    Revert startup commands to only install umami

--- .gitpod/drupal-setup.sh
+++ .gitpod/drupal-setup.sh
@@ -3,12 +3,4 @@ set -x
 
 # Install Drupal Umami
 ddev composer install
-ddev drush si -y --account-pass=admin --site-name="FLDC-demo"
-
-# Set foomami as default theme
-ddev drush then -y foomami
-ddev drush config-set -y system.theme default foomami
-
-# Claro admin theme
-ddev drush then -y claro
-ddev drush config-set -y system.theme admin claro
+ddev drush si demo_umami -y
